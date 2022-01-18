73df80a6aec7bab499c7001940ee221ef498d037
ZimnyCat <thezimnycat@gmail.com>
2022-01-18 21:22:28 +0500

    add AirPlace

--- src/main/java/wtf/cattyn/ferret/api/manager/impl/ModuleManager.java
+++ src/main/java/wtf/cattyn/ferret/api/manager/impl/ModuleManager.java
@@ -1,21 +1,19 @@
 package wtf.cattyn.ferret.api.manager.impl;
 
-import org.luaj.vm2.LuaValue;
 import wtf.cattyn.ferret.api.feature.module.Module;
-import wtf.cattyn.ferret.api.feature.script.Script;
-import wtf.cattyn.ferret.api.feature.script.lua.classes.ModuleLua;
 import wtf.cattyn.ferret.api.manager.Manager;
+import wtf.cattyn.ferret.impl.features.modules.AirPlace;
 import wtf.cattyn.ferret.impl.features.modules.Sprint;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public final class ModuleManager extends ArrayList<Module> implements Manager<ModuleManager> {
 
     @Override public ModuleManager load() {
         addAll(List.of(
-                new Sprint()
+                new Sprint(),
+                new AirPlace()
         ));
         return this;
     }
