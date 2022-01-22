fd104e7add7eaf3f98d3d833705af11160a844da
ZimnyCat <thezimnycat@gmail.com>
2022-01-22 22:10:04 +0500

    add rotate option to KillAura

--- src/main/java/wtf/cattyn/ferret/impl/features/modules/KillAura.java
+++ src/main/java/wtf/cattyn/ferret/impl/features/modules/KillAura.java
@@ -4,12 +4,15 @@ import com.google.common.eventbus.Subscribe;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.util.Hand;
 import wtf.cattyn.ferret.api.feature.module.Module;
+import wtf.cattyn.ferret.api.feature.option.impl.BooleanOption;
 import wtf.cattyn.ferret.api.feature.option.impl.NumberOption;
+import wtf.cattyn.ferret.common.impl.util.WorldUtil;
 import wtf.cattyn.ferret.impl.events.TickEvent;
 
 public class KillAura extends Module {
-    // TODO: more options
+
     NumberOption d = new NumberOption.Builder(4.5f).name("range").setBounds(1f, 10f).build(this);
+    BooleanOption r = new BooleanOption.Builder(false).name("rotate").build(this);
 
     public KillAura() {
         super("KillAura", "Automatically attacks players", Category.COMBAT);
@@ -21,11 +24,18 @@ public class KillAura extends Module {
         mc.world.getPlayers().stream()
                 .filter(p -> p.isAlive() && mc.player.distanceTo(p) <= d.getValue().floatValue() && p != mc.player).forEach(p -> {
                     boolean s = mc.player.isSprinting();
+                    Float[] pre = new Float[]{};
+
                     if (s) mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.STOP_SPRINTING));
+                    if (r.getValue()) pre = WorldUtil.lookAt(p.getBlockX(), p.getBlockY(), p.getBlockZ());
 
                     mc.interactionManager.attackEntity(mc.player, p);
                     mc.player.swingHand(Hand.MAIN_HAND);
 
+                    if (r.getValue()) {
+                        mc.player.setYaw(pre[0]);
+                        mc.player.setPitch(pre[1]);
+                    }
                     if (s) mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.START_SPRINTING));
                 });
     }
